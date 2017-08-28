2301db57d5506472c499abb1c8c2b61f0fa093c8
andrew ananta <kuro.ketanitem@gmail.com>
2017-08-29 01:22:34 +0700

    1. add utils for retreive data that translate camelCase query name into database snakecase fields name

--- schema/types/me.js
+++ schema/types/me.js
@@ -5,10 +5,15 @@ const {
   GraphQLString
 } = require('graphql');
 
+const { fromSnakeCase } = require('../../lib/util');
+
 module.exports = new GraphQLObjectType({
   name : "MeType",
   fields : {
     id : { type : GraphQLID },
+    firstName : fromSnakeCase(GraphQLString),
+    lastName : fromSnakeCase(GraphQLString),
+    createdAt : fromSnakeCase(GraphQLString),
     email : { type : new GraphQLNonNull(GraphQLString) }
   }
 });
