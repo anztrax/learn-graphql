63428f426d3d6d623e1c0726e5e3e6df7b51298b
andrew ananta <kuro.ketanitem@gmail.com>
2017-08-31 19:26:57 +0700

    1. add new contest, contestStatus 2. add new query to get all contests

--- database/accessors/pgdb.js
+++ database/accessors/pgdb.js
@@ -11,6 +11,14 @@ module.exports = pgPool => {
         console.log('res : ',res.rows);
         return humps.camelizeKeys(res.rows[0]);
       });
+    },
+    getContests(user){
+      return pgPool.query(`
+        select * from contests
+        where created_by = $1
+      `,[user.id]).then(res => {
+        return humps.camelizeKeys(res.rows);
+      });
     }
   }
 };
--- schema/types/me.js
+++ schema/types/me.js
@@ -2,8 +2,12 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLNonNull,
-  GraphQLString
+  GraphQLString,
+  GraphQLList
 } = require('graphql');
+
+const ContestType = require('./contest');
+const pgdb = require('../../database/accessors/pgdb');
 
 module.exports = new GraphQLObjectType({
   name : "MeType",
@@ -16,6 +20,13 @@ module.exports = new GraphQLObjectType({
       resolve : obj => `${obj.firstName} ${obj.lastName}`
     },
     createdAt : { type : GraphQLString },
-    email : { type : new GraphQLNonNull(GraphQLString) }
+    email : { type : new GraphQLNonNull(GraphQLString) },
+    contests : {
+      type : new GraphQLList(ContestType),
+      resolve(obj, args, ctx){
+        const { pgPool } = ctx;
+        return pgdb(pgPool).getContests(obj);
+      }
+    }
   }
 });
